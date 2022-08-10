bf6f39bfe9022f9775a2025b5ccab88d62cf2aaf
SHpat Spahiu <shpat.spahiu99@gmail.com>
2022-08-10 10:32:13 +0200

    doge pics with axios

--- src/App.js
+++ src/App.js
@@ -4,21 +4,51 @@ import Contact from './Pages/Contact';
 import axios from 'axios'
 import React from 'react';
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+// const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+// export default function App() {
+//   const [post, setPost] = React.useState(null);
+
+//   React.useEffect(() => {
+//     axios.get(baseURL).then((response) => {
+//       setPost(response.data);
+//     });
+//   }, []);
+//   if (!post) return null;
+
+//   return (
+//     <div>
+//       <h1>{post.title}</h1>
+//       <p>{post.body}</p>
+//     </div>
+//   );
+// }
+
+
+const baseURL = "https://dog.ceo/api/breeds/image/random";
 export default function App() {
   const [post, setPost] = React.useState(null);
 
-  React.useEffect(() => {
+
+
+  function changeImage() {
     axios.get(baseURL).then((response) => {
       setPost(response.data);
-    });
+    })
+  }
+
+  React.useEffect(() => {
+    changeImage()
   }, []);
+
   if (!post) return null;
 
+
   return (
+
+
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      <img width="400" src={post.message}></img>
+      <button onClick={changeImage}>Next</button>
     </div>
   );
 }
